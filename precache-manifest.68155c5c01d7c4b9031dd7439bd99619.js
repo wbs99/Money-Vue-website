d3500af9ceec4cda3131e11ff5abc525d607f612
wbs <1134954328@qq.com>
2021-09-12 21:46:58 +0800

    deploy

---
+++ precache-manifest.68155c5c01d7c4b9031dd7439bd99619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847dafe0001e4b4e8d9b",
+    "url": "/Money-Vue-website/css/app.07934a81.css"
+  },
+  {
+    "revision": "caa5afd7666df37b7302c382a2bcaae8",
+    "url": "/Money-Vue-website/index.html"
+  },
+  {
+    "revision": "847dafe0001e4b4e8d9b",
+    "url": "/Money-Vue-website/js/app.c556f810.js"
+  },
+  {
+    "revision": "1be455e0c888af71fb9e",
+    "url": "/Money-Vue-website/js/chunk-vendors.83141f2f.js"
+  },
+  {
+    "revision": "8743e3c6d3a9deb93a5a625838948152",
+    "url": "/Money-Vue-website/manifest.json"
+  },
+  {
+    "revision": "010547ab9326e494c8a57c84ba5b4920",
+    "url": "/Money-Vue-website/qrrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Money-Vue-website/robots.txt"
+  }
+]);
